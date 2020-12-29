4e9213e9c3cb4dffc509ff85758115496c3f4302
bartoszkruba <bartosz.kruba@gmail.com>
2020-12-29 18:38:42 +0100

    Solutions for day one

---
+++ day_1/main.py
@@ -0,0 +1,26 @@
+inputs = []
+for line in open('puzle_input.txt'):
+    inputs.append(int(line))
+
+
+# Part one
+def find_solution1(inputs):
+    for i in range(len(inputs)):
+        for j in range(i + 1, len(inputs)):
+            if inputs[i] + inputs[j] == 2020:
+                return inputs[i] * inputs[j]
+
+
+print('Part one solution:', find_solution1(inputs))
+
+
+# Part two
+def find_solution2(inputs):
+    for i in range(len(inputs)):
+        for j in range(i + 1, len(inputs)):
+            for k in range(j + 1, len(inputs)):
+                if inputs[i] + inputs[j] + inputs[k] == 2020:
+                    return inputs[i] * inputs[j] * inputs[k]
+
+
+print('Part two solution:', find_solution2(inputs))
