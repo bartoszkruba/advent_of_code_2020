939bba2b5be8a3efbeaeeed413095b83e8290225
bartoszkruba <bartosz.kruba@gmail.com>
2020-12-31 13:30:25 +0100

    Solutions for day four, part two

--- day_4/main.py
+++ day_4/main.py
@@ -1,3 +1,5 @@
+import re
+
 inputs = []
 
 with open('puzzle_inputs.txt') as f:
@@ -21,15 +23,72 @@ def parse_passports(inputs):
     return passports
 
 
-def find_solution1(passports):
-    def passport_is_valid(passport):
-        for key in ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']:
-            if key not in passport:
+def filter_passwords(passports, rules):
+    def filter_fun(passport):
+        for field in rules:
+            if field not in passport:
                 return False
+            for rule in rules[field]:
+                if rule == 'length' and len(passport[field]) != rules[field][rule]:
+                    return False
+                if rule == 'min' and int(passport[field]) < rules[field][rule]:
+                    return False
+                if rule == 'max' and int(passport[field]) > rules[field][rule]:
+                    return False
+                if rule == 'regex' and re.compile(str(rules[field][rule])).match(passport[field]) is None:
+                    return False
+                if rule == 'in' and passport[field] not in rules[field][rule]:
+                    return False
+                if rule == 'custom' and rules[field][rule](passport[field]) is False:
+                    return False
         return True
 
-    return len(list(filter(passport_is_valid, passports)))
+    return list(filter(filter_fun, passports))
+
+
+def find_solution1(passports):
+    rules = {'byr': {}, 'iyr': {}, 'eyr': {}, 'hgt': {}, 'hcl': {}, 'ecl': {}, 'pid': {}}
+
+    return len(filter_passwords(passports, rules))
 
 
 passports = parse_passports(inputs)
 print("Part one solution: ", find_solution1(passports))
+
+
+def find_solution2(passports):
+    rules = {
+        'byr': {
+            'length': 4,
+            'min': 1920,
+            'max': 2002
+        },
+        'iyr': {
+            'length': 4,
+            'min': 2010,
+            'max': 2020
+        },
+        'eyr': {
+            'length': 4,
+            'min': 2020,
+            'max': 2030
+        },
+        'hgt': {
+            'regex': '^[0-9]{3}cm|[0-9]{2}in$',
+            'custom': lambda x: (150 <= int(x[:-2]) <= 193) if x[-2:] == 'cm' else (59 <= int(x[:-2]) <= 76)
+        },
+        'hcl': {
+            'regex': '^#[0-9a-f]{6}$'
+        },
+        'ecl': {
+            'in': ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        },
+        'pid': {
+            'regex': '^[0-9]{9}$'
+        }
+    }
+
+    return len(filter_passwords(passports, rules))
+
+
+print("Part two solution: ", find_solution2(passports))
