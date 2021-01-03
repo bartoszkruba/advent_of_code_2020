9533dc9847739ae2258cbab4f91ec50d2d76c1cf
bartoszkruba <bartosz.kruba@gmail.com>
2021-01-03 13:04:56 +0100

    Solution for day 8, part two

--- day_8/main.py
+++ day_8/main.py
@@ -3,15 +3,15 @@ with open('puzzle_inputs.txt') as f:
 
 
 def parse_commands(inputs):
-    return [(input.split(' ')[0], int(input.split(' ')[1])) for input in inputs]
+    return [[input.split(' ')[0], int(input.split(' ')[1])] for input in inputs]
 
 
-def find_solution1(commands):
+def run_commands(commands):
     indexes = []
     index = 0
     accumulator = 0
 
-    while index not in indexes:
+    while index not in indexes and index < len(commands):
         command = commands[index][0]
         value = commands[index][1]
 
@@ -24,9 +24,28 @@ def find_solution1(commands):
 
         indexes.append(index)
         index += 1
-    return accumulator
+
+    return accumulator, True if index == len(commands) else False
+
+
+def find_solution1(commands):
+    return run_commands(commands)[0]
+
+
+def find_solution2(commands):
+    for i in range(len(commands)):
+        command = commands[i][0]
+        if command in ['jmp', 'nop']:
+            commands[i][0] = 'nop' if command == 'jmp' else 'jmp'
+            accumulator, correct = run_commands(commands)
+            if correct:
+                return accumulator
+            else:
+                commands[i][0] = command
 
 
 commands = parse_commands(inputs)
 
 print('Part one solution: ', find_solution1(commands))
+
+print('Part two solution: ', find_solution2(commands))
