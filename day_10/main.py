cd510512cb882954b7e5f2a5e64afc5a3c35b0cd
bartoszkruba <bartosz.kruba@gmail.com>
2021-01-03 22:40:13 +0100

    Solution for day 10, part two.

--- day_10/main.py
+++ day_10/main.py
@@ -25,4 +25,33 @@ def find_solution1(inputs):
     return differences[1] * differences[3]
 
 
+def find_possible_next_adapters(number, inputs):
+    possibilities = []
+    for possibility in [number + 1, number + 2, number + 3]:
+        if possibility in inputs:
+            possibilities.append(possibility)
+    return possibilities
+
+
+# Counting combination by creating "node tree"
+# Each step in while loop is a next level in the tree
+def find_solution2(inputs):
+    count = 0
+
+    nodes = {input: 1 if input == 0 else 0 for input in [0] + inputs}
+
+    while sum(nodes.values()) != 0:
+        count += nodes[inputs[-1]]
+        new_nodes = {input: 0 for input in [0] + inputs}
+
+        for node in nodes:
+            for number in find_possible_next_adapters(node, inputs):
+                new_nodes[number] += nodes[node]
+        nodes = new_nodes
+
+    return count
+
+
 print('Part one solution:', find_solution1(inputs))
+
+print('Part two solution:', find_solution2(inputs))
