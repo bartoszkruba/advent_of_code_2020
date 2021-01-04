95fbaf2c4aed2c0d52557736668524bef1bd2249
bartoszkruba <bartosz.kruba@gmail.com>
2021-01-04 16:18:44 +0100

    Solution for day 12, part two.

--- day_12/main.py
+++ day_12/main.py
@@ -8,6 +8,20 @@ def parse_commands(inputs):
     return [(input[0], int(input[1:])) for input in inputs]
 
 
+def rotate(deg, pos):
+    if deg % 180 != 0:
+        pos[0], pos[1] = pos[1], pos[0]
+
+    if deg == 90:
+        pos[1] *= -1
+    elif deg == 180:
+        pos[0] *= -1
+        pos[1] *= -1
+    elif deg == 270:
+        pos[0] *= -1
+    return pos
+
+
 def find_solution1(commands):
     direction = 90
     pos = [0, 0]
@@ -35,6 +49,32 @@ def find_solution1(commands):
     return abs(pos[0]) + abs(pos[1])
 
 
+def find_solution2(commands):
+    pos = [0, 0]
+    wp = [10, 1]
+
+    for command in commands:
+        instruction = command[0]
+        value = command[1]
+        if instruction == 'N':
+            wp[1] += value
+        elif instruction == 'S':
+            wp[1] -= value
+        elif instruction == 'E':
+            wp[0] += value
+        elif instruction == 'W':
+            wp[0] -= value
+        elif instruction == 'L':
+            rotate(360 - value, wp)
+        elif instruction == 'R':
+            rotate(value, wp)
+        elif instruction == 'F':
+            pos[0] += value * wp[0]
+            pos[1] += value * wp[1]
+    return abs(pos[0]) + abs(pos[1])
+
+
 commands = parse_commands(inputs)
 
 print("Part one solution: ", find_solution1(commands))
+print("Part two solution: ", find_solution2(commands))
