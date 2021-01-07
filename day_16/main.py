0aef44ef5b0df246f03580642c1484f362f6160f
bartoszkruba <bartosz.kruba@gmail.com>
2021-01-07 17:04:59 +0100

    Solution for day 16, part two

--- day_16/main.py
+++ day_16/main.py
@@ -40,6 +40,12 @@ def invalid_values(ticket, rules):
     return invalid_values
 
 
+def index_with_single_label(possibilities, ignore=[]):
+    for key in possibilities:
+        if len(possibilities[key]) == 1 and key not in ignore:
+            return key
+
+
 def find_solution1(tickets, rules):
     all_invalid_values = []
     for ticket in tickets:
@@ -47,4 +53,44 @@ def find_solution1(tickets, rules):
     return sum(all_invalid_values)
 
 
-print('Part one solution:', find_solution1(others_tickets, rules))
+def find_solution2(own_ticket, others_tickets, rules):
+    valid_tickets = []
+    for ticket in others_tickets:
+        if len(invalid_values(ticket, rules)) == 0:
+            valid_tickets.append(ticket)
+    valid_tickets.append(own_ticket)
+
+    possibilities = {}
+    for i in range(len(own_ticket)):
+        possibilities[i] = [key for key in rules]
+
+    for i in range(len(own_ticket)):
+        for key in rules:
+            r = rules[key]
+            for ticket in valid_tickets:
+                value = ticket[i]
+                valid = valid_for_some_rule(value, r)
+                if not valid:
+                    possibilities[i].remove(key)
+                    break
+
+    ignore = []
+    while index_with_single_label(possibilities, ignore) is not None:
+        index = index_with_single_label(possibilities, ignore)
+        ignore.append(index)
+        label = possibilities[index][0]
+        for key in possibilities:
+            if key not in ignore and label in possibilities[key]:
+                possibilities[key].remove(label)
+
+    result = 1
+    for key in possibilities:
+        label = possibilities[key][0]
+        if label.startswith('departure'):
+            result *= own_ticket[key]
+
+    return result
+
+
+print('Solution for part one:', find_solution1(others_tickets, rules))
+print('Solution for part two:', find_solution2(own_ticket, others_tickets, rules))
