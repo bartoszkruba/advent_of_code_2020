c0428b94ffc453c33bae090df30ae14e06ea086c
bartoszkruba <bartosz.kruba@gmail.com>
2021-01-08 18:10:31 +0100

    Solution for day 17, part two

--- day_17/main.py
+++ day_17/main.py
@@ -15,6 +15,13 @@ def create_cube(shape, content=0):
     return cube
 
 
+def create_hyper_cube(shape, content=0):
+    cube = []
+    for w in range(shape[0]):
+        cube.append(create_cube(shape[1:]))
+    return cube
+
+
 def create_permutations():
     permutations = []
 
@@ -26,14 +33,17 @@ def create_permutations():
     return permutations
 
 
-# def print_cube(cube):
-#     z = 0
-#     for arr in cube:
-#         print('\n')
-#         print('z:', z - middle_z)
-#         z += 1
-#         for line in arr:
-#             print(line)
+def create_4d_permutations():
+    permutations = []
+
+    for w in (0, -1, 1):
+        for z in (0, -1, 1):
+            for y in (0, -1, 1):
+                for x in (0, -1, 1):
+                    permutations.append((w, y, x, z))
+    permutations.remove((0, 0, 0, 0))
+
+    return permutations
 
 
 def neighbours_count(coordinates, permutations, cube):
@@ -49,6 +59,19 @@ def neighbours_count(coordinates, permutations, cube):
     return count
 
 
+def neighbours_count_4d(coordinates, permutations, cube):
+    count = 0
+    for p in permutations:
+        try:
+            if cube[coordinates[0] + p[0]][coordinates[1] + p[1]][coordinates[2] + p[2]][coordinates[3] + p[3]] == 1:
+                count += 1
+            if count >= 4:
+                return count
+        except:
+            pass
+    return count
+
+
 def active_field_count(cube):
     total = 0
 
@@ -56,6 +79,17 @@ def active_field_count(cube):
         for y in range(len(cube[0])):
             for x in range(len(cube[0][0])):
                 total += cube[z][y][x]
+    return total
+
+
+def active_field_count_4d(cube):
+    total = 0
+
+    for w in range(len(cube)):
+        for z in range(len(cube[0])):
+            for y in range(len(cube[0][0])):
+                for x in range(len(cube[0][0][0])):
+                    total += cube[w][z][y][x]
     return total
 
 
@@ -98,4 +132,45 @@ def find_solution1(inputs):
     return active_field_count(cube)
 
 
+def find_solution2(inputs):
+    cycles = 6
+    shape = [1 + 2 * cycles, 1 + 2 * cycles, len(inputs) + 2 * cycles, len(inputs[0]) + 2 * cycles]
+    cube = create_hyper_cube(shape)
+    middle_w = int(shape[0] / 2)
+    middle_z = int(shape[1] / 2)
+    middle_y = int(shape[2] / 2)
+    middle_x = int(shape[3] / 2)
+
+    for y in range(middle_y - int(len(inputs) / 2), middle_y - int(len(inputs) / 2) + len(inputs)):
+        for x in range(middle_x - int(len(inputs[0]) / 2), middle_y - int(len(inputs) / 2) + len(inputs)):
+            little_y = y - (middle_y - int(len(inputs) / 2))
+            little_x = x - (middle_x - int(len(inputs[0]) / 2))
+            cube[middle_w][middle_z][y][x] = 1 if inputs[little_y][little_x] == '#' else 0
+
+    new_cube = create_hyper_cube(shape)
+    permutations = create_4d_permutations()
+
+    for n in range(cycles):
+        for w in range(shape[0]):
+            for z in range(shape[1]):
+                for y in range(shape[2]):
+                    for x in range(shape[3]):
+                        coordinates = (w, z, y, x)
+                        count = neighbours_count_4d(coordinates, permutations, cube)
+                        value = cube[w][z][y][x]
+
+                        if count < 2:
+                            new_value = 0
+                        elif value == 1 and count in [2, 3]:
+                            new_value = 1
+                        elif value == 0 and count == 3:
+                            new_value = 1
+                        else:
+                            new_value = 0
+                        new_cube[w][z][y][x] = new_value
+        cube, new_cube = new_cube, cube
+    return active_field_count_4d(cube)
+
+
 print('Part one solution:', find_solution1(inputs))
+print('Part two solution:', find_solution2(inputs))
